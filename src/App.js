4171cb8a7fb0bceb9546b9d743290f9d680d89cf
markuslaine <markus.laine@aalto.fi>
2021-05-09 14:21:36 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React jeejee
+          Learn React
         </a>
       </header>
     </div>
