0e08025f0753cf0a260caeda5d6abab063018262
markuslaine <markus.laine@aalto.fi>
2021-05-11 14:10:01 +0300

    Added styling to App.css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,12 +12,12 @@ export default function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        <h3>#OSANASUUREMPAA</h3>
+                        <h3 className="monospace">#OSANASUUREMPAA</h3>
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />       
                     </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
                         <li className='nav-item'>
                             <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                                 Koti
