cdc29d1d104b1adaf042915dd1a7698ea3605251
Antti Norrkniivilä <antti.norrkniivila@aalto.fi>
2021-05-11 11:50:52 +0300

    routed Frontpage to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Frontpage from './components/frontpage';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact />
+        <Route path='/' exact component={Frontpage}/>
       </Switch>
     </Router>
     </>
