ee312f67c5313af5940246da9de9170e7146d93b
Antti Norrkniivilä <antti.norrkniivila@aalto.fi>
2021-05-12 11:14:11 +0300

    worked on questioncards

--- src/components/cardpage.js
+++ src/components/cardpage.js
@@ -1,10 +1,29 @@
 import React, {useState} from 'react'
 import './cardpage.css'
 import Questioncard from './questioncard'
+import Answercard from './answercard'
 
 export default function Cardcontainer() {
     const [questionNumber, setQuestionNumber] = useState(1)
     const [points, setPoints] = useState(0)
+    const [answercard, setAnswercard] = useState(false)
+    const [wasRight, setWasRight] = useState()
+
+    const question = <Questioncard 
+                        src='images/img-vaccine.jpg' 
+                        text='Totta vai tarua? Koronavirusrokotteessa on 5G-mikrosiruja.'
+                        rightAnswer='false' 
+                        setAnswercard={setAnswercard}
+                        setWasRight={setWasRight}
+                        
+                    />
+    const answer = <Answercard />
+    
+    const [visible, setVisible] = useState(answercard ? answer : question)
+
+    
+
+    
     return (
         <>
         <h1>TESTAA TIETOSI KORONAROKOTTEESTA</h1>
@@ -15,10 +34,7 @@ export default function Cardcontainer() {
                     <h3>Pisteet: {points}</h3>
                 </div>
                 <div className='card-changing'>
-                    <Questioncard 
-                        src='images/img-vaccine.jpg' 
-                        text='Totta vai tarua? Koronavirusrokotteessa on 5G-mikrosiruja.' 
-                    />
+                    {visible}
                 </div>
             </div>
         </div>
